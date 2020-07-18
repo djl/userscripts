d99bd552fab5e4f031d4a595077dbfdf32f122f4
David Logie <djl@mksh.org>
2020-07-18 08:52:36 +0100

    Style improvements.

--- musicbrainz-atisket.js
+++ musicbrainz-atisket.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Musicbrainz Atisket
+// @name         Musicbrainz a-tisket
 // @description  Add a-tisket links to iTunes, Apple Music and Spotify URLs
 // @version      2020.07.08.1
 // @namespace    github.com/djl
@@ -7,14 +7,13 @@
 // @include      http*://*musicbrainz.org/*
 // ==/UserScript==
 
-
 (function() {
     var re = new RegExp('musicbrainz.org/release/([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})', 'i');
     if (!window.location.href.match(re)) {
         return;
     }
 
-    document.querySelectorAll('div#bottom-credits a').forEach(function(link) {
+    document.querySelectorAll('div#bottom-credits a').forEach(function (link) {
         if (link.href.match(/deezer.com|(music|itunes).apple.com|spotify.com/)) {
             var id;
             var fragment;
@@ -31,12 +30,13 @@
             }
 
             var next = link.nextElementSibling.nextElementSibling;
-            var newlink = document.createElement('a')
+            var newlink = document.createElement('a');
             newlink.href = `https://etc.marlonob.info/atisket/?${fragment}_id=${id}`;
             newlink.text = 'a-tisket';
 
             next.before(document.createTextNode(' ['));
             next.before(newlink);
             next.before(document.createTextNode(']'));
-        }});
+        }
+    });
 })();
