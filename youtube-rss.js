1525a858365b1ce79f7bcba57d8b633cd0677fa7
David Logie <djl@mksh.org>
2021-02-19 21:03:52 +0000

    youtube-rss: Update.
    This makes the script work more consistently but I'm not 100% sure why.

--- youtube-rss.js
+++ youtube-rss.js
@@ -2,23 +2,27 @@
 // @name        YouTube RSS Feed
 // @namespace   https://github.com/djl/userscripts
 // @author      djl
-// @version     2020.09.20.1
+// @version     2020.11.06.1
 // @description Add an RSS feed link to channel pages
 // @match       *://www.youtube.com/*
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
 
-const base_url = 'http://www.youtube.com/feeds/videos.xml?channel_id=';
-
 (function () {
     'use strict';
-    const urls = document.querySelectorAll('meta[property="og:url"]');
-    if (urls.length == 0) {
-        return;
+    const base_url = 'http://www.youtube.com/feeds/videos.xml?channel_id=';
+    let channel;
+    let url = document.querySelector('meta[itemprop="channelId"]');
+    if (url) {
+        channel = url.content;
+    } else {
+        let urls = document.querySelectorAll('meta[property="og:url"]');
+        if (urls.length == 0) {
+            return;
+        }
+        channel = urls[0].content.split(/channel\//);
     }
-    const url = urls[0].content;
-    const channel = url.split(/channel\//)[1];
     const sub = document.getElementById('subscribe-button');
     const rss = document.createElement('a');
     rss.id = 'rss-button';
@@ -30,5 +34,5 @@ const base_url = 'http://www.youtube.com/feeds/videos.xml?channel_id=';
     rss.style.fontSize = '14px';
     rss.style.textDecoration = 'none';
     rss.style.marginRight = '5px';
-    sub.before(rss);
+    sub.parentNode.insertBefore(rss, sub);
 })();
