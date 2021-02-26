4c9949d9402dd9139043525b602add7c9cf282f8
David Logie <djl@mksh.org>
2021-02-26 17:29:05 +0000

    musicbrainz-copy-as-directory: Fixes.

--- musicbrainz-copy-as-directory.js
+++ musicbrainz-copy-as-directory.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Musicbrainz: Copy release info as directory
 // @description  Copy release info as directory. Useful when combined with EAC/XLD/whipper
-// @version      2021.02.24.1
+// @version      2021.02.26.1
 // @namespace    github.com/djl
 // @author       djl
 // @grant        GM_setClipboard
@@ -9,14 +9,18 @@
 // ==/UserScript==
 (function () {
     const js = document.querySelector("script[type='application/ld+json']");
+    if (!js) {
+        return;
+    }
     const json = JSON.parse(js.innerText);
     let cats = '';
     if (json.catalogNumber) {
         if (Array.isArray(json.catalogNumber)) {
-            cats = ` {${json.catalogNumber.join(', ')}}`;
+            cats = `${json.catalogNumber.join(', ')}`;
         } else if (json.catalogNumber != '[none]') {
-            cats = json.catalogNumber;
+            cats = `${json.catalogNumber}`;
         }
+        cats = ` {${cats}}`;
     }
     const date = json.hasReleaseRegion[0].releaseDate.split('-')[0];
     const fmt = `${json.releaseOf.creditedTo} - ${json.releaseOf.name} (${date}) [FLAC]${cats}`;
