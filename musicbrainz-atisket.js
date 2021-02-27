ca1a3ee95820186c77e8680299ae68df3a51dc68
David Logie <djl@mksh.org>
2021-02-27 19:22:51 +0000

    musicbrainz-atisket: Fix bug on pages with multiple links.
    If a page had multiple relevant links, all the new atisket links would
    be created next to the last link.

--- musicbrainz-atisket.js
+++ musicbrainz-atisket.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Musicbrainz a-tisket
 // @description  Add a-tisket links to iTunes, Apple Music and Spotify URLs
-// @version      2021.02.15.1
+// @version      2021.02.27.1
 // @namespace    github.com/djl
 // @author       djl
 // @include      http*://*musicbrainz.org/*
 // ==/UserScript==
 
-const atiskets = ['https://atisket.pulsewidth.org.uk/', 'https://etc.marlonob.info/atisket/'];
+const atiskets = ['https://atisket.pulsewidth.org.uk', 'https://etc.marlonob.info/atisket'];
 
 (function () {
     const re = new RegExp('musicbrainz.org/release/([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})', 'i');
@@ -31,13 +31,19 @@ const atiskets = ['https://atisket.pulsewidth.org.uk/', 'https://etc.marlonob.in
                 fragment = 'spf';
             }
 
-            const next = link.parentElement.lastChild;
-            for (const i in atiskets) {
-                const url = atiskets[i];
-                const newlink = document.createElement('a');
-                newlink.href = `${url}/?${fragment}_id=${id}`;
-                newlink.text = 'a-tisket';
-                next.before(' [', newlink, ']');
+            let sibling = link.nextElementSibling;
+            while (sibling) {
+                if (sibling.matches('br')) {
+                    for (const i in atiskets) {
+                        const url = atiskets[i];
+                        const newlink = document.createElement('a');
+                        newlink.href = `${url}/?${fragment}_id=${id}`;
+                        newlink.text = 'a-tisket';
+                        sibling.before(' [', newlink, ']');
+                    }
+                    break;
+                }
+                sibling = sibling.nextElementSibling;
             }
         }
     });
