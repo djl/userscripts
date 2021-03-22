dcda2d6a3a5713c58268d880bf57083ae223077e
David Logie <djl@mksh.org>
2021-03-22 17:10:17 +0000

    musicbrainz-copy-as-directory: Remove unwanted chars from output.

--- musicbrainz-copy-as-directory.js
+++ musicbrainz-copy-as-directory.js
@@ -1,12 +1,17 @@
 // ==UserScript==
 // @name         Musicbrainz: Copy release info as directory
 // @description  Copy release info as directory. Useful when combined with EAC/XLD/whipper
-// @version      2021.02.26.1
+// @version      2021.03.20.1
 // @namespace    github.com/djl
 // @author       djl
 // @grant        GM_setClipboard
-// @include      /^https?:\/\/(\w+\.)?musicbrainz\.org\/release\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}(\/disc\/\d+#[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})?/
+// @include      /^https?:\/\/(\w+\.)?musicbrainz\.org\/release\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/?
 // ==/UserScript==
+
+function clean(s) {
+    return s.replace(/[<>:/\\*?"]/g, '_');
+}
+
 (function () {
     const js = document.querySelector("script[type='application/ld+json']");
     if (!js) {
@@ -23,7 +28,7 @@
         cats = ` {${cats}}`;
     }
     const date = json.hasReleaseRegion[0].releaseDate.split('-')[0];
-    const fmt = `${json.releaseOf.creditedTo} - ${json.releaseOf.name} (${date}) [FLAC]${cats}`;
+    const fmt = `${json.releaseOf.creditedTo} - ${json.name} (${date}) [FLAC]${cats}`;
     const list = document.querySelector('div.tabs ul.tabs');
     const li = document.createElement('li');
     const lia = document.createElement('a');
@@ -32,7 +37,14 @@
     li.appendChild(lia);
     list.appendChild(li);
     lia.addEventListener('click', function (event) {
-        GM_setClipboard(fmt);
+        GM_setClipboard(clean(fmt));
+        li.classList.add('sel');
+        const txt = lia.text;
+        lia.text = 'Copied!';
+        setTimeout(function () {
+            li.classList.remove('sel');
+            lia.text = txt;
+        }, 3000);
         event.preventDefault();
     });
 })();
