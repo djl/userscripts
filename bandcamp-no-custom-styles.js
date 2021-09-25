e246cbe431b6b559bece87e36acbcd66a3ba1aac
David Logie <djl@mksh.org>
2021-09-25 11:51:25 +0100

    bandcamp-no-custom-styles: Fix includes for ViolentMonkey.

--- bandcamp-no-custom-styles.js
+++ bandcamp-no-custom-styles.js
@@ -2,14 +2,14 @@
 // @name         Bandcamp: No custom styles
 // @description  Gets rid of an artist's custom styles
 // @namespace    github.com/djl
-// @version      0.1
+// @version      2021-09-24.1
 // @author       djl
 // @include      /^https?://[^/]+/(?:album|track)/[^/]+$/
-// @include      /^https?://(.*)\.bandcamp\.com/[^/]+$/
+// @include      /^https?://(.*)\.bandcamp\.com/
 // @run-at       document-body
 // ==/UserScript==
-
 (function () {
     'use strict';
     document.querySelector('style#custom-design-rules-style').remove();
+    document.querySelector('body').classList.remove('invertIconography');
 })();
