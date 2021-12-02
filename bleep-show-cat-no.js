e6f8a46e36981fa3863042053d413cdf0389d03b
David Logie <djl@mksh.org>
2021-12-02 13:10:57 +0000

    Add bleep-show-cat-no.js.

---
+++ bleep-show-cat-no.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Bleep: Display cat no.
+// @namespace    github.com/djl
+// @version      2021-12-01.1
+// @description  Display cat no. next to label
+// @author       djl
+// @include      /^https://bleep\.com/release/(.*)/
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    const label = document.querySelector('div.product-info dd.label');
+    const catno = document.querySelector('div.product-info dd.catalogue-number');
+    console.log(label);
+    console.log(catno);
+    if (label == null || catno == null) {
+        return;
+    }
+    label.append(` (${catno.textContent.trim()})`);
+})();
