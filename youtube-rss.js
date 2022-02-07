099105930b871a48ddddb6f1b73b704510590e44
David Logie <djl@mksh.org>
2022-02-07 13:17:50 +0000

    youtube-rss: Add slight delay before running.

--- youtube-rss.js
+++ youtube-rss.js
@@ -2,7 +2,7 @@
 // @name        YouTube RSS Feed
 // @namespace   github.com/djl/userscripts
 // @author      djl
-// @version     2020.11.06.1
+// @version     2022.02.06.1
 // @description Add an RSS feed link to channel pages
 // @match       *://www.youtube.com/*
 // @grant       none
@@ -23,16 +23,19 @@
         }
         channel = urls[0].content.split(/channel\//);
     }
-    const sub = document.getElementById('subscribe-button');
-    const rss = document.createElement('a');
-    rss.id = 'rss-button';
-    rss.href = base_url + channel;
-    rss.innerHTML = 'RSS';
-    rss.style.backgroundColor = '#fd9b12';
-    rss.style.padding = '10px 16px';
-    rss.style.color = '#ffffff';
-    rss.style.fontSize = '14px';
-    rss.style.textDecoration = 'none';
-    rss.style.marginRight = '5px';
-    sub.parentNode.insertBefore(rss, sub);
+    window.setTimeout(function () {
+        const sub = document.getElementById('subscribe-button');
+        const rss = document.createElement('a');
+        rss.id = 'rss-button';
+        rss.href = base_url + channel;
+        rss.innerHTML = 'RSS';
+        rss.style.backgroundColor = '#fd9b12';
+        rss.style.padding = '10px 16px';
+        rss.style.color = '#ffffff';
+        rss.style.fontSize = '14px';
+        rss.style.fontWeight = 'bold';
+        rss.style.textDecoration = 'none';
+        rss.style.marginRight = '5px';
+        sub.parentNode.insertBefore(rss, sub);
+    }, 1000);
 })();
