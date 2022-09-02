a588d1c5bc0e42f22d9b1d83f7739a9715513bc1
David Logie <djl@mksh.org>
2022-09-02 12:50:34 +0100

    cue-trumps: Don't change things already marked as trumpable.

--- cue-trumps.js
+++ cue-trumps.js
@@ -1,26 +1,27 @@
 // ==UserScript==
 // @name         Cue Trumps
 // @description  Show "Trumpable" Tag for torrents that have a 100% log and no cue sheet.
-// @version      2021-09-09-01
-// @namespace    github.com/djl/userscripts
+// @version      2022-09-02-01
+// @namespace    github.com/djl
 // @author       djl
-// @include      https://orpheus.network/artist.php?id=*
-// @include      https://orpheus.network/torrents.php?id=*
-// @include      https://redacted.ch/artist.php?id=*
-// @include      https://redacted.ch/torrents.php?id=*
+// @match        *://orpheus.network/*
+// @match        *://redacted.ch/*
 // @run-at       document-idle
 // ==/UserScript==
 
 (function () {
     'use strict';
+    if (!/(artists|torrents).php\?id/.test(window.location)) {
+        return;
+    }
     let torrents = document.querySelectorAll('div.torrent_info');
     if (torrents.length == 0) {
         torrents = document.querySelectorAll('tr.torrent_row td:first-child > a');
     }
     torrents.forEach(torrent => {
         const text = torrent.innerText;
-        if (text.includes('Log (100%)') && !text.includes('Cue')) {
-            torrent.querySelector('strong').innerText += ' (Trumpable)';
+        if (text.includes('Log (100%)') && !text.includes('Cue') && !text.includes('Trumpable')) {
+            torrent.querySelector('strong').innerText += ' [TRUMPABLE!]';
         }
     });
 })();
