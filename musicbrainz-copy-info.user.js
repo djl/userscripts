67417c2c2d0152075efbc6fd79979a7a4572ab09
David Logie <djl@mksh.org>
2023-01-19 17:13:36 +0000

    Simplify and improve musicbrainz-copy-as-directory.

---
+++ musicbrainz-copy-info.user.js
@@ -0,0 +1,90 @@
+// ==UserScript==
+// @name         Musicbrainz: Copy release info.
+// @description  Copy release info from MusicBrainz
+// @version      2023.01.19.01
+// @namespace    github.com/djl/userscripts
+// @author       djl
+// @updateURL    https://raw.githubusercontent.com/djl/userscripts/master/musicbrainz-copy-info.user.js
+// @downloadURL  https://raw.githubusercontent.com/djl/userscripts/master/musicbrainz-copy-info.user.js
+// @grant        GM_setClipboard
+// @match        *://musicbrainz.org/release/*
+// @match        *://musicbrainz.org/release-group/*
+// ==/UserScript==
+
+// Unwanted catalog numbers
+// MB really needs a "this release has no cat no." option
+const unwantedCats = ['[none]'];
+
+function clean(s) {
+    return s.replace(/[|<>:/\\*?"]/g, '_');
+}
+
+function credits(json) {
+    if (json.creditedTo) {
+        return json.creditedTo;
+    } else {
+        return json.releaseOf.creditedTo;
+    }
+}
+
+function cat(json) {
+    if (!json.catalogNumber) {
+        return '';
+    }
+    let r = json.catalogNumber;
+    if (Array.isArray(json.catalogNumber)) {
+        r = json.catalogNumber.filter(function (elem) {
+            return elem && !unwantedCats.includes(elem);
+        })[0];
+    } else if (unwantedCats.includes(r)) {
+        return '';
+    }
+    return ` {${r}}`;
+}
+
+function addButton(text, func) {
+    const list = document.querySelector('div.tabs ul.tabs');
+    const li = document.createElement('li');
+    const lia = document.createElement('a');
+    lia.href = '#';
+    lia.text = text;
+    li.appendChild(lia);
+    list.appendChild(li);
+    lia.addEventListener('click', function (event) {
+        li.classList.add('sel');
+        func(event);
+        lia.text = 'Copied!';
+        setTimeout(function () {
+            li.classList.remove('sel');
+            lia.text = text;
+        }, 3000);
+        event.preventDefault();
+    });
+}
+
+(function () {
+    if (window.location.href.includes('/release/')) {
+        addButton('Copy as Directory', function () {
+            const json = JSON.parse(document.querySelector("script[type='application/ld+json']").textContent);
+            const cats = `${cat(json)}`;
+            const date = json.hasReleaseRegion[0].releaseDate.split('-')[0];
+            const fmt = `${credits(json)} - ${json.name} (${date}) [FLAC]${cats}`;
+            GM_setClipboard(clean(fmt));
+        });
+
+        addButton('Copy Release ID', function () {
+            GM_setClipboard(window.location.href.split('/').pop());
+        });
+
+        addButton('Copy Group ID', function () {
+            let a = document.querySelector('p.subheader span.small a');
+            GM_setClipboard(a.href.split('/').pop());
+        });
+    }
+
+    if (window.location.href.includes('/release-group/')) {
+        addButton('Copy Group ID', function () {
+            GM_setClipboard(window.location.href.split('/').pop());
+        });
+    }
+})();
