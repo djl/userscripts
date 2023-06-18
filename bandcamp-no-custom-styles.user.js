e3ad7719514be6bd1acbacf9481126ed9666e1b7
David Logie <djl@mksh.org>
2023-06-18 16:22:58 +0100

    bandcamp-no-custom-styles: Actually run on Bandcamp.

--- bandcamp-no-custom-styles.user.js
+++ bandcamp-no-custom-styles.user.js
@@ -2,10 +2,11 @@
 // @name         Bandcamp: No custom styles
 // @description  Gets rid of an artist's custom styles
 // @namespace    github.com/djl/userscripts
-// @version      2023.06.17.01
+// @version      2023.06.18.01
 // @author       djl
 // @updateURL    https://raw.githubusercontent.com/djl/userscripts/master/bandcamp-no-custom-styles.user.js
 // @downloadURL  https://raw.githubusercontent.com/djl/userscripts/master/bandcamp-no-custom-styles.user.js
+// @match        *://*.bandcamp.com/*
 // @match        *://*/album/*
 // @match        *://*/track/*
 // @run-at       document-body
